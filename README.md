bfb8a3d36c6f6a1f119c1e58792fa72577f15f36
Chaiyaphop Jamjumrat <j.chaiyaphop@gmail.com>
2024-05-09 22:00:28 +0700

    update led_scraping.py for accessing more provifnces and update README.md to align new led_scraping.py

--- README.md
+++ README.md
@@ -8,7 +8,10 @@ LED_Scraping requires Google Chrome 120.0.6099.130, Python 3.11.3 and Anaconda V
 - Install Anaconda Virtual Environment (env.yml) or all those packages in requirements file (requirements.txt).
 - Activate Anaconda Virtual Environment.
 - Run led_scraping.py to extract data from LED's website.
-- Type the BKK Civil (integer) you want to extract data.
+- Type the asset type id (string) you want to extract data.
+- Type the province (string) you want to extract data.
+    - If you want to extract data from BKK.
+        - Type the BKK Civil (integer) you want to extract data.
 - The result will be in "output" folder.
 
 ***Chaiyaphop Jamjumrat. (2023, Jun 3). LED_Scraping: Legal Execution Department's website Scraping in Python.***
--- led_scraping.py
+++ led_scraping.py
@@ -6,7 +6,7 @@ import pandas as pd
 import time
 
 
-def find_n_page(civils):
+def find_n_page(urls, civils):
     # Set up the Chrome driver
     options = webdriver.ChromeOptions()
     options.add_argument('--headless')
@@ -14,20 +14,19 @@ def find_n_page(civils):
     driver = webdriver.Chrome(
             options=options
         )
-
+    
     # Find the number of page
     n_page = {}
-    for civil in civils:
-        url = f'https://asset.led.go.th/newbid-old/asset_search_province.asp?search_asset_type_id=002&search_province=%A1%C3%D8%A7%E0%B7%BE&search_sub_province=%E1%BE%E8%A7%A1%C3%D8%A7%E0%B7%BE%C1%CB%D2%B9%A4%C3%20{civil}'
+    for i, url in enumerate(urls):
         driver.get(url)
         find_n_page = driver.find_element(by=By.XPATH, value='/html/body/table[3]/tbody/tr/td[1]/table[2]/tbody/tr/td[2]/div')
         page_nums = list(find_n_page.text.split('/'))[1]
-        n_page[civil] = int(page_nums)
-
+        n_page[civils[i]] = int(page_nums)
     driver.quit()
+
     return n_page
 
-def scrape_data(n_page, output_dir, output_filename):
+def scrape_data(urls, n_page, output_dir, output_filename):
     # Set up the Chrome driver
     options = webdriver.ChromeOptions()
     options.add_argument('--headless')
@@ -38,10 +37,11 @@ def scrape_data(n_page, output_dir, output_filename):
 
     # Scrape data
     data_dict = {}
-    for c in n_page:
+    for i, c in enumerate(n_page):
         data_list = []
+        url = urls[i]
         for page in range(1, int(n_page[c])+1):
-            sub_url = f'https://asset.led.go.th/newbid-old/asset_search_province.asp?search_asset_type_id=002&search_province=%A1%C3%D8%A7%E0%B7%BE&search_sub_province=%E1%BE%E8%A7%A1%C3%D8%A7%E0%B7%BE%C1%CB%D2%B9%A4%C3%20{c}&page={page}'
+            sub_url = f'{url}&page={page}'
             driver.get(sub_url)
             time.sleep(0.5)
             try:
@@ -105,7 +105,7 @@ def scrape_data(n_page, output_dir, output_filename):
 
                     home_windows = driver.window_handles[0]
                     driver.switch_to.window(home_windows)
-                    time.sleep(0.5)
+                    time.sleep(0.1)
             except:
                 None
         data_dict[c] = data_list
@@ -117,6 +117,7 @@ def scrape_data(n_page, output_dir, output_filename):
             df.to_excel(writer, sheet_name=f'CIVIL_{data}', index=False)
 
     driver.quit()
+
     return data_dict
 
 
@@ -124,25 +125,50 @@ if __name__ == '__main__':
     start_time = datetime.now()
     print('Program start at %s' % start_time)
 
-    # Select BKK civil
-    selected_civil = []
-    while True:
-        civil = input('(int) Which BKK civil do you want to scrape? type "e" to exit. ')
-        if civil == 'e':
-            break
-        else:
-            selected_civil.append(int(civil))
-    print(f'Civil {selected_civil}')
-
     # Define variables
-    output_dir = 'Projects/led_scraping/output'
-    gen_date = (datetime.now()).strftime('%Y%m%d')
-    output_filename = f'LED_SCRAPING_{gen_date}.xlsx'
-    # # Find number of page
-    n_page = find_n_page(selected_civil)
+    prefix = 'https://asset.led.go.th/newbid-old/asset_search_province.asp'
+    asset_type_id = input('(str) Which asset type id do you want to scrape? 002 = ห้องชุด, 003 = ที่ดินพร้อมสิ่งปลูกสร้าง ')
+    province = input('(str) Which province do you want to scrape? bkk = กรุงเทพ, spk = สมุทรปราการ, pte = ปทุมธานี')
+    urls = []
+    if province == 'bkk':
+        province_id = '%A1%C3%D8%A7%E0%B7%BE' # กรุงเทพ
+        # Select BKK civil
+        civils = []
+        while True:
+            civil = input('(int) Which BKK civil do you want to scrape? type "e" to exit. ')
+            if civil == 'e':
+                break
+            else:
+                civils.append(int(civil))
+        print(f'Civil {civils}')
+        for civil in civils:
+            sub_province_code = f'%E1%BE%E8%A7%A1%C3%D8%A7%E0%B7%BE%C1%CB%D2%B9%A4%C3%20{civil}'
+            url = f'{prefix}?search_asset_type_id={asset_type_id}&search_province={province_id}&search_sub_province={sub_province_code}'
+            urls.append(url)
+    elif province == 'spk':
+        province_code = '%CA%C1%D8%B7%C3%BB%C3%D2%A1%D2%C3' # สมุทรปราการ
+        civils = [0]
+        print(f'Civil {civils}')
+        url = f'{prefix}?search_asset_type_id={asset_type_id}&search_province={province_code}'
+        urls.append(url)
+    elif province == 'pte':
+        province_code = '%BB%B7%D8%C1%B8%D2%B9%D5' # ปทุมธานี
+        civils = [0]
+        print(f'Civil {civils}')
+        url = f'{prefix}?search_asset_type_id={asset_type_id}&search_province={province_code}'
+        urls.append(url)
+    print(urls)
+
+    # Find number of page
+    n_page = find_n_page(urls, civils)
 
     # Scrape data
-    _ = scrape_data(n_page, output_dir, output_filename)
+    # # Define variables
+    output_dir = 'Projects/led_scraping/output'
+    gen_date = (datetime.now()).strftime('%Y%m%d')
+    output_filename = f'LED_{province}_SCRAPING_{gen_date}.xlsx'
+
+    _ = scrape_data(urls, n_page, output_dir, output_filename)
 
     end_time = datetime.now()
     print('Program end at : ', end_time)
